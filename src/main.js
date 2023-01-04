09a6421cfae507779c2730f71986b764b6a9906d
David Ljubic <dl53150@fer.hr>
2023-01-04 18:34:24 +0100

    fixed bootstrap css and js paths

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import "bootstrap/dist/css/bootstrap.css";
+import "../../node_modules/bootstrap/dist/css/bootstrap.css";
 import { createApp } from 'vue'
 import './styles/style.css'
 import App from './App.vue'
@@ -6,4 +6,4 @@ import App from './App.vue'
 
 createApp(App).mount('#app')
 
-import "bootstrap/dist/js/bootstrap.js";
+import "../../node_modules/bootstrap/dist/js/bootstrap.js";
