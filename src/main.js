0ed2b0c702d16ece0e289d0da27df33cbf7063af
David Ljubic <dl53150@fer.hr>
2023-01-04 19:55:53 +0100

    ready to deploy

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import "../../node_modules/bootstrap/dist/css/bootstrap.css";
-import { createApp } from 'vue'
+import { createApp } from '../../node_modules/vue'
 import './styles/style.css'
 import App from './App.vue'
 
